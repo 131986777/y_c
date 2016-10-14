5e04369764af92841f31c5813011c7b35ed35a3f
zhaocheng12 <1633611801@qq.com>
2016-10-14 14:27:56 +0800

    资金、积分（Bug修改）

--- babuluo/web/app/point/point/point-services.js
+++ babuluo/web/app/point/point/point-services.js
@@ -1,13 +1,4 @@
 AndSellMainModule.service('pointFactory', function ($resource, baseURL) {
-    this.modMemberAccount = function (form) {
-        console.log(form);
-        return $resource(baseURL + '/member/account/modifyById', form, {
-            'update': {
-                method: 'PUT'
-            }
-        });
-    };
-
     this.addFinanceList= function (form) {
         return $resource(baseURL + '/member/point/add', form, {
             'update': {
