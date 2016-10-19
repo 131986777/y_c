4943e0ea5956bd5246279428780f02adaa98d1f5
zhaocheng12 <1633611801@qq.com>
2016-10-19 17:31:19 +0800

    促销显示

--- babuluo/web/app/marketing/sales/sales-services.js
+++ babuluo/web/app/marketing/sales/sales-services.js
@@ -1,7 +1,6 @@
 AndSellMainModule.service('salesFactory', function ($resource, baseURL) {
 
     this.ModifySalesState= function (form) {
-        console.log(form);
         return $resource(baseURL + '/sales/sales/modifyById', form, {
             'update': {
                 method: 'PUT'
