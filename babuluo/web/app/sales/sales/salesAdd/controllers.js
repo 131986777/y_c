c70a5831cafc3acf9fc17cf27f960e66b1bccacc
snail <wyp402905456@hotmail.com>
2016-10-19 09:12:01 +0800

    salesAdd

--- babuluo/web/app/sales/sales/salesAdd/controllers.js
+++ babuluo/web/app/sales/sales/salesAdd/controllers.js
@@ -1,21 +1,25 @@
 AndSellMainModule.controller('salesAddController', function ($scope, $stateParams, salesFactory, modalFactory) {
     $(function(){
+
         var len = $('.radio').length;
         for(var i = 0 ; i<len;i++){
             $('.radio').eq(i).click(function(){
                 //点击显示
                 $(this).children('.content').removeClass('hidden');
                 $(this).siblings().children('.content').addClass('hidden');
-                var detail_html = $(this).children('.detailBox').html();
-                $(this).children('.addItem').click(function(){
-                    $('.detailBox').append(detail_html);
-
-                });
             });
         }
+
         $('.addItem').on("click",function(){
-            var detail_html = $(this).siblings('.detailBox').html();
-            $(this).siblings('.detailBox').append(detail_html);
+            var a = $(this).parents('.content'),
+                b = a.find('.template').clone();
+            b.removeClass("template hidden").addClass("detail");
+            var c = a.find('.detailBox');
+            c.append(b);
+            $('.itemDel').on('click',function(){
+                $(this).parents('.detail').remove();
+
+            });
         });
 
     })
