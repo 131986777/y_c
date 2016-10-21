7faca22df65356633b8dbac70db111fa147e0bfb
zhaocheng12 <1633611801@qq.com>
2016-10-21 16:06:52 +0800

    新增促销规则

--- babuluo/web/app/marketing/sales/rule/ruleAdd/controllers.js
+++ babuluo/web/app/marketing/sales/rule/ruleAdd/controllers.js
@@ -1,69 +1,46 @@
 AndSellMainModule.controller('salesRuleAddController', function ($scope,$http, $stateParams, salesFactory, modalFactory) {
     modalFactory.setBottom(true);
 
-
-
-
     $scope.memberId = $stateParams.id;
 
-    /*function loadInputData(form){
+    $scope.bindData = function(form){
+        form['SALES.PROID'] = $scope.product;
+        if(form['SALES.SALE_TYPE'] ==3||form['SALES.SALE_TYPE'] ==4){
+            form['SALES.SALE_CONTENT1'] = '{'+'"ProId"'+':'+form['SALES.PROID']+','
+                                             +'"Num"'+':'+form['SALES.PRONUM']+'}';
+        }
+        if(form['SALES.CONDITION_TYPE'] ==true){
+            form['SALES.CONDITION_TYPE'] =2;
+        }else {
+            form['SALES.CONDITION_TYPE'] =1;
+        }
         form['SALES.SALE_TARGET'] = $scope.memberId;
-        form['SALES.IS_DEL'] = -1;
-        form['SALES.STATE'] = 1;   //启用
         return form;
-    }*/
-
+    }
 
     //设置页面Bottom触发事件
     modalFactory.setBottom(true, function () {
-        //var form = loadInputData($scope.salesInfo);
-        console.log("++++++");
+        //if ($scope.salesInfo['SALES.NAME'] == undefined ||salesInfo['SALES.INTRO']) {
+            //modalFactory.showAlert("请填写完整信息");
+            //return;
+        //}
+        //else{
+        var form = $scope.bindData($scope.salesInfo);
         console.log($scope.salesInfo);
         salesFactory.AddSales (form).get({}, function (response) {
-            if (response.code != undefined && (response.code == 4000 || response.code == 400)) {
-                modalFactory.showShortAlert(response.msg);
-            } else if (response.extraData.state == 'true') {
-
-            }
-        });
-
-        //验证
-        /*if ($scope.memberData['MEMBER_INFO.TRUE_NAME'] == undefined) {
-         modalFactory.showAlert("真实姓名不能为空。");
-         return;
-         }
-         if ($scope.ADDR_SHENG != undefined && $scope.ADDR_SHENG != '') {
-
-         $scope.memberData['MEMBER_INFO.ADDR_SHENG']= $scope.ADDR_SHENG.p;
-         if ($scope.ADDR_SHI != undefined) {
-         $scope.memberData['MEMBER_INFO.ADDR_SHI'] = $scope.ADDR_SHI.n;
-         }
-         if ($scope.ADDR_XIAN != undefined) {
-         $scope.memberData['MEMBER_INFO.ADDR_XIAN']= $scope.ADDR_XIAN.s;
-         }
-         }
-         memberFactory.modMemberDataById($scope.memberData).get({}, function (response) {
-         if (response.code != undefined && (response.code == 4000 || response.code == 400)) {
-         modalFactory.showShortAlert(response.msg);
-         } else if (response.extraData.state == 'true') {
-         modalFactory.showShortAlert("保存成功");
-         // $scope.modifyID = false;
-         $scope.initLoad();
-         }
-         });
-         // console.log('要提交的数据为'+$scope.memberData);
-
-         }, function () {
-         //取消事件
-         $scope.initLoad();*/
+                if (response.code != undefined && (response.code == 4000 || response.code == 400)) {
+                    modalFactory.showShortAlert(response.msg);
+                } else if (response.extraData.state == 'true') {
+                    modalFactory.showShortAlert("保存成功");
+                    $scope.$broadcast('pageBar.reload');
+                }
+            });
+        //}
     });
 
-
-
-
-
-
-
+    $scope.prdItemSwitch= function (data) {
+        $scope.product = data['SHOP_PRODUCT_SKU.PRD_ID'];
+    }
 
     //前端Jquery逻辑
     $(function(){
@@ -99,8 +76,5 @@ AndSellMainModule.controller('salesRuleAddController', function ($scope,$http, $
 
             });
         });
-
-
     })
-
 });
