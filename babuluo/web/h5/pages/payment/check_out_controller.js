2db3d184812d5d432a5e6a902bce6588baac677c
xq <269015753@qq.com>
2016-10-22 14:31:36 +0800

    移动端页面第一批遗漏

---
+++ babuluo/web/h5/pages/payment/check_out_controller.js
@@ -0,0 +1,19 @@
+AndSellH5MainModule.controller('H5.OrderSuccessController', function ($scope, $state,$stateParams,productFactory,orderFactory,modalFactory) {
+
+    modalFactory.setTitle('下单成功');
+    modalFactory.setBottom(false);
+
+    $scope.initData= function () {
+        orderFactory.getById($stateParams.ORDER_ID).get({}, function (response) {
+            response.data[0]['SHOP_ORDER.DATETIME_ADD']=getDate(response.data[0]['SHOP_ORDER.DATETIME_ADD']);
+            $scope.order=response.data[0];
+        });
+    }
+
+    //支付
+    $scope.payNow= function () {
+
+    }
+
+
+})
