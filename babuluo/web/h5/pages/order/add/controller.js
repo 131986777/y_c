cceb9523634e83bd38e4632f7369f4e126e0a706
snail <wyp402905456@hotmail.com>
2016-11-09 10:54:07 +0800

    找回密码页面、移动端显示异常修正

--- babuluo/web/h5/pages/order/add/controller.js
+++ babuluo/web/h5/pages/order/add/controller.js
@@ -38,6 +38,65 @@ AndSellH5MainModule.controller('H5.OrderAddController', function ($scope, $state
         $scope.shop = JSON.parse(getCookie('currentShopInfo'));
 
     }
+    var defaults;
+
+    var show = function(html, className) {
+
+        className = className || "";
+        var mask = $("<div class='weui_mask_transparent'></div>").appendTo(document.body);
+
+        var tpl = '<div class="weui_toast ' + className + '">' + html + '</div>';
+        var dialog = $(tpl).appendTo(document.body);
+
+        dialog.show();
+        dialog.addClass("weui_toast_visible");
+    };
+
+    var hide = function(callback) {
+        $(".weui_mask_transparent").remove();
+        $(".weui_toast_visible").removeClass("weui_toast_visible").transitionEnd(function() {
+            var $this = $(this);
+            $this.remove();
+            callback && callback($this);
+        });
+    }
+
+    $.toast = function(text, style, callback) {
+        if(typeof style === "function") {
+            callback = style;
+        }
+        var className;
+        if(style == "cancel") {
+            className = "weui_toast_cancel";
+        } else if(style == "forbidden") {
+            className = "weui_toast_forbidden";
+        } else if(style == "text") {
+            className = "weui_toast_text";
+        }
+        show('<i class="weui_icon_toast"></i><p class="weui_toast_content">' + (text || "已经完成") + '</p>', className);
+
+        setTimeout(function() {
+            hide(callback);
+        }, toastDefaults.duration);
+    }
+
+    $.showLoading = function(text) {
+        var html = '<div class="weui_loading">';
+        for(var i=0;i<12;i++) {
+            html += '<div class="weui_loading_leaf weui_loading_leaf_' + i + '"></div>';
+        }
+        html += '</div>';
+        html += '<p class="weui_toast_content">' + (text || "数据加载中") + '</p>';
+        show(html, 'weui_loading_toast');
+    }
+
+    $.hideLoading = function() {
+        hide();
+    }
+
+    var toastDefaults = $.toast.prototype.defaults = {
+        duration: 2000
+    }
 
     //计算订单价格  未做优惠促销等逻辑
     $scope.updateCartPrice= function () {
@@ -93,5 +152,6 @@ AndSellH5MainModule.controller('H5.OrderAddController', function ($scope, $state
 
         });
     }
+
 
 });
