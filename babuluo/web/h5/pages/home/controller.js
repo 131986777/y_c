00121282d532331d2073a2dc3c07863aaaa1ceb6
xq <269015753@qq.com>
2016-11-09 10:12:24 +0800

    一大波

--- babuluo/web/h5/pages/home/controller.js
+++ babuluo/web/h5/pages/home/controller.js
@@ -8,11 +8,8 @@ AndSellH5MainModule.controller('H5.HomeController', function ($scope, $state, we
         $state.go('prd-List', {keyword: $scope.prdKeyword});
     }
 
-    var slides;
-
     $scope.initData = function () {
         var id = getCookie('currentShop');
-        console.log(id);
         if (id == undefined || id.trim() == '') {
             weUI.toast.info('请先选择门店');
             $state.go('shopList');
