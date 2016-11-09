abc282a8311f6c93cf1e0f63eab5119fe8625d18
xq <269015753@qq.com>
2016-11-09 15:13:52 +0800

    more

--- babuluo/web/h5/pages/order/add/controller.js
+++ babuluo/web/h5/pages/order/add/controller.js
@@ -7,7 +7,7 @@ angular.module('AndSell.H5.Main').controller('pages_order_add_Controller', funct
 
     $scope.initData= function () {
 
-        $scope.PickupPerson = $stateParams.pickupPerson;
+        $scope.PickupPerson = JSON.parse($stateParams.pickupPerson);
 
         $scope.EmptyPick = isEmptyObject($scope.PickupPerson);
 
@@ -37,65 +37,6 @@ angular.module('AndSell.H5.Main').controller('pages_order_add_Controller', funct
 
         $scope.shop = JSON.parse(getCookie('currentShopInfo'));
 
-    }
-    var defaults;
-
-    var show = function(html, className) {
-
-        className = className || "";
-        var mask = $("<div class='weui_mask_transparent'></div>").appendTo(document.body);
-
-        var tpl = '<div class="weui_toast ' + className + '">' + html + '</div>';
-        var dialog = $(tpl).appendTo(document.body);
-
-        dialog.show();
-        dialog.addClass("weui_toast_visible");
-    };
-
-    var hide = function(callback) {
-        $(".weui_mask_transparent").remove();
-        $(".weui_toast_visible").removeClass("weui_toast_visible").transitionEnd(function() {
-            var $this = $(this);
-            $this.remove();
-            callback && callback($this);
-        });
-    }
-
-    $.toast = function(text, style, callback) {
-        if(typeof style === "function") {
-            callback = style;
-        }
-        var className;
-        if(style == "cancel") {
-            className = "weui_toast_cancel";
-        } else if(style == "forbidden") {
-            className = "weui_toast_forbidden";
-        } else if(style == "text") {
-            className = "weui_toast_text";
-        }
-        show('<i class="weui_icon_toast"></i><p class="weui_toast_content">' + (text || "已经完成") + '</p>', className);
-
-        setTimeout(function() {
-            hide(callback);
-        }, toastDefaults.duration);
-    }
-
-    $.showLoading = function(text) {
-        var html = '<div class="weui_loading">';
-        for(var i=0;i<12;i++) {
-            html += '<div class="weui_loading_leaf weui_loading_leaf_' + i + '"></div>';
-        }
-        html += '</div>';
-        html += '<p class="weui_toast_content">' + (text || "数据加载中") + '</p>';
-        show(html, 'weui_loading_toast');
-    }
-
-    $.hideLoading = function() {
-        hide();
-    }
-
-    var toastDefaults = $.toast.prototype.defaults = {
-        duration: 2000
     }
 
     //计算订单价格  未做优惠促销等逻辑
@@ -133,7 +74,7 @@ angular.module('AndSell.H5.Main').controller('pages_order_add_Controller', funct
             params['SHOP_ORDER.REC_TYPE']=2;//收货方式为自提
             params['SHOP_ORDER.SHOP_NAME']=$scope.shop['SHOP.SHOP_NAME'];//门店信息
             params['SHOP_ORDER.SHOP_ID']=$scope.shop['SHOP.SHOP_ID'];//门店ID
-            params['SHOP_ORDER.GET_PRD_DATETIME']=$scope.PickupPerson.time;//提货时间
+            params['SHOP_ORDER.GET_PRD_DATETIME']=$scope.PickupPerson.getTime;//提货时间
         }
 
         params['SHOP_ORDER.DETAILS']=JSON.stringify($scope.skuList);//sku信息
