048e65a6d9555fa393860e463a9d3da839c82c92
caoxingyu <caoxingyu2016@qq.com>
2016-11-09 16:05:00 +0800

    我的优惠券

--- babuluo/web/h5/pages/coupon/list/controller.js
+++ babuluo/web/h5/pages/coupon/list/controller.js
@@ -1,8 +1,8 @@
-angular.module('AndSell.H5.Main').controller('pages_coupon_list_Controller', function ($scope, $state, couponFactory, modalFactory) {
+angular.module('AndSell.H5.Main').controller('pages_coupon_list_Controller', function ($scope, $state, couponFactory,personalFactory, modalFactory) {
 
     modalFactory.setTitle('领券中心');
     // modalFactory.setBottom(true);
-
+    $scope.couponSumMap=new Map();
 
     $scope.initData = function () {
         console.log('初始化数据');
@@ -10,16 +10,25 @@ angular.module('AndSell.H5.Main').controller('pages_coupon_list_Controller', fun
             $scope.couponList = response.data;
             console.log($scope.couponList);
         });
+        $scope.saveCouponNum();
 
-        var add={};
-        add['MEMBER_COUPON.USER_ID'] =1000;
-        couponFactory.queryCouponListByUser(add).get({}, function (response) {
-            $scope.memberCouponList = response.data;
-            console.log($scope.memberCouponList);
-        });
         }
 
+    $scope.saveCouponNum=function () {
+        var member={};
+        member['MEMBER_COUPON.USER_ID'] =1000;
+        personalFactory.getCouponListByUser(member).get({}, function (response) {
+            var memberCouponList = response.data;
+            console.log(memberCouponList);
+            memberCouponList.forEach(function (ele) {
+                $scope.couponSumMap.set(ele['MEMBER_COUPON.COUPON_ID'],ele['.NUM_COUPON']);  //将客户所拥有的各优惠券的数量存在map中
+            });
+
+        });
+    }
+
     $scope.initData();
+
 
     $scope.detailClick = function (item) {
         // $scope.detail = item;
@@ -37,36 +46,46 @@ angular.module('AndSell.H5.Main').controller('pages_coupon_list_Controller', fun
     $scope.addCoupon = function (item) {
         $scope.coupon =item;
 
-        $scope.add['MEMBER_COUPON.COUPON_ID'] = $scope.coupon['COUPON.ID'];
-        $scope.add['MEMBER_COUPON.EXPIRED_TIME'] = $scope.coupon['COUPON.END_DATETIME'];
-        $scope.add['MEMBER_COUPON.USER_ID'] =1000;
+        var sum=$scope.couponSumMap.get($scope.coupon['COUPON.ID']);  //客户所拥有的当前优惠券的数量
+        var count=$scope.coupon['COUPON.RULE_INFO']['COUPON_RULE.EACH_MEMBER_LIMIT'];  //该优惠券所允许的每人领取的最大数量
+        console.log(sum);
+        console.log(count);
+        if(sum!=undefined&&sum>=count){
+            alert('该优惠券的领取数量已经达到了上限，换张再领吧！');
+        }
+       else {
+            $scope.add['MEMBER_COUPON.COUPON_ID'] = $scope.coupon['COUPON.ID'];
+            $scope.add['MEMBER_COUPON.EXPIRED_TIME'] = $scope.coupon['COUPON.END_DATETIME'];
+            $scope.add['MEMBER_COUPON.USER_ID'] = 1000;
 
-        $scope.coupon['COUPON.NUM_LEFT'] = $scope.coupon['COUPON.NUM_LEFT'] - 1;
+            $scope.coupon['COUPON.NUM_LEFT'] = $scope.coupon['COUPON.NUM_LEFT'] - 1;
 
 
-        couponFactory.addMemberCoupon($scope.add).get({}, function (response) {
+            couponFactory.addMemberCoupon($scope.add).get({}, function (response) {
 
-            if (response.code == 400) {
-                modalFactory.showShortAlert(response.msg);
+                if (response.code == 400) {
+                    modalFactory.showShortAlert(response.msg);
 
-            } else if (response.extraData.state == 'true') {
+                } else if (response.extraData.state == 'true') {
 
-                couponFactory.modCouponLeft($scope.coupon).get({}, function (response) {  //修改优惠券剩余数量
-                    if (response.code == 400) {
-                        modalFactory.showShortAlert(response.msg);
+                    couponFactory.modCouponLeft($scope.coupon).get({}, function (response) {  //修改优惠券剩余数量
+                        if (response.code == 400) {
+                            modalFactory.showShortAlert(response.msg);
 
-                    } else if (response.extraData.state == 'true') {
-                        alert('领取成功!')
-                        $scope.add = '';
+                        } else if (response.extraData.state == 'true') {
 
-                        //$scope.$broadcast('pageBar.reload');
-                    }
+                            alert('领取成功!')
+                            $scope.initData();
+                            $scope.add = '';
 
-                });
+                        }
 
-            }
+                    });
 
-        });
+                }
+
+            });
+        }
 
     };
 
