bf02fccf854aaea8737ab38730f8a011bef94cb7
xq <269015753@qq.com>
2016-11-10 23:55:54 +0800

    test

--- babuluo/web/app/login/controllers.js
+++ babuluo/web/app/login/controllers.js
@@ -1,7 +1,6 @@
 angular.module('app',[]).controller('login_Controller', function ($scope,$http) {
 
     $scope.login = function () {
-
         ajaxPost('http://localhost:8081/AndSell/bubu/login/login','LOGIN_ID='+$scope.userName+'&PWD='+$scope.password, function (response) {
             if (response.code == 400) {
               alert(response.msg);
