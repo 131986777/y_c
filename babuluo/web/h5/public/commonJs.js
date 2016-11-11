5c7eeed414cb8288fd1ae6fae911dfc8df6ba1f7
xq <269015753@qq.com>
2016-11-11 19:43:17 +0800

    test

--- babuluo/web/h5/public/commonJs.js
+++ babuluo/web/h5/public/commonJs.js
@@ -284,16 +284,16 @@ function Map() {
 }
 
 var updateWxTitle = function(title) {
-    var body = document.getElementsByTagName('body')[0];
-    document.title = title;
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "/empty.png");
-
-    iframe.addEventListener('load', function() {
-        setTimeout(function() {
-            iframe.removeEventListener('load');
-            document.body.removeChild(iframe);
-        }, 0);
-    });
-    document.body.appendChild(iframe);
+    //var body = document.getElementsByTagName('body')[0];
+    //document.title = title;
+    //var iframe = document.createElement("iframe");
+    //iframe.setAttribute("src", "/empty.png");
+    //
+    //iframe.addEventListener('load', function() {
+    //    setTimeout(function() {
+    //        iframe.removeEventListener('load');
+    //        document.body.removeChild(iframe);
+    //    }, 0);
+    //});
+    //document.body.appendChild(iframe);
 }
