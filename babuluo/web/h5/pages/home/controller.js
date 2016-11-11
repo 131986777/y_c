f454150cf1843d13dd2c9d51971ce6208a2e7688
xq <269015753@qq.com>
2016-11-11 18:35:34 +0800

    test

--- babuluo/web/h5/pages/home/controller.js
+++ babuluo/web/h5/pages/home/controller.js
@@ -1,4 +1,4 @@
-angular.module('AndSell.H5.Main').controller('pages_home_Controller', function ($scope, $state, weUI, modalFactory, shopFactory) {
+angular.module('AndSell.H5.Main').controller('pages_home_Controller', function ($interval,$scope, $state, weUI, modalFactory, shopFactory) {
 
     modalFactory.setTitle('主页');
     modalFactory.setBottom(true);
@@ -48,18 +48,17 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
                     $scope.limitList.push(ele);
                     //获取到限时抢购的开始时间和结束时间
                     var startTime=new Date(ele['BANNER.BEGIN_DATETIME'].split('.')[0]).getTime();    //开始时间
-                    var  endTime=new Date(ele['BANNER.END_DATETIME'].split('.')[0]).getTime();      //结束时间
+                    var endTime=new Date(ele['BANNER.END_DATETIME'].split('.')[0]).getTime();      //结束时间
                     var currentTime=new Date().getTime();      //当前时间 毫秒
 
-                    setInterval(function () {
+                    $scope.timer=$interval(function(){
                         if(currentTime>startTime&&currentTime<endTime){   //如果当前时间在活动时间内
                             currentTime+=1000;
                             fomtime(endTime-startTime);
                         }else{
-                         clearInterval();
+                            $interval.cancel($scope.timer);
                         }
-
-                    },1000);
+                    },1000);   //间隔2秒定时执行
 
                 }
                 if (ele['BANNER.POSITION_ID']==1014){    //  团购
@@ -83,39 +82,19 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
 
     }
 
-
-            //以毫秒为单位
-   var  fomtime = function  (a)    //实现倒计时
-    {
-        var b=new Date();
-        b.setTime(0);
-        var c=new Date();
-        c.setTime(a);
-        var day1=b.getDate();        //为方便调用，把天数、小时等单独定义
-        var hours1=b.getHours();
-        var minu1=b.getMinutes();
-        var seco1=b.getSeconds();
-        var day2=c.getDate();
-        var hours2=c.getHours();
-        var minu2=c.getMinutes();
-        var seco2=c.getSeconds();
-        var day=day2-day1;
-        var hours=hours2-hours1;
-        var minu=minu2-minu1;
-        var seco=seco2-seco1;
-        console.log(day);
-        $scope.day=day;
-        $scope.hour=hours;
-        $scope.minute=minu;
-        $scope.second=seco;
-        console.log(7988);
-        console.log($scope.day);
-
+   //以毫秒为单位
+   var fomtime = function(date){
+        var d=new Date(new Date(date)-new Date());
+        $scope.day=d.getDate();
+        $scope.hour=d.getHours();
+        $scope.minute=d.getMinutes();
+        $scope.second=d.getSeconds();
 
     }
 
-
-
-
+    $scope.$on('destroy',function(){
+        $interval.cancel($scope.timer);
+    })
 
 });
+
