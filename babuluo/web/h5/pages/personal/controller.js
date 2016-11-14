d9c396cb13c2df9698a2da47af8b469542abdb49
zhaocheng12 <1633611801@qq.com>
2016-11-14 12:22:59 +0800

    Merge branch 'master' of D:\Project\bbl with conflicts.

--- babuluo/web/h5/pages/personal/controller.js
+++ babuluo/web/h5/pages/personal/controller.js
@@ -8,5 +8,13 @@ angular.module('AndSell.H5.Main').controller('pages_personal_Controller', functi
             $state.go('pages/user/accountLogin');
         });
     }
-
+    $scope.getSession = function () {
+        userFactory.getSession().get({}, function (response) {
+            if (response.code == 400) {
+                weUI.toast.error(response.msg);
+            } else {
+                console.log(response);
+            }
+        });
+    }
 });
