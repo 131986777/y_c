c5f65bfce816cefd6ce7138a80650f15be88f388
pabula <50054447@qq.com>
2016-11-14 23:31:36 +0800

    一问题

--- babuluo/web/app/login/controllers.js
+++ babuluo/web/app/login/controllers.js
@@ -4,7 +4,14 @@ angular.module('app',[]).controller('login_Controller', function ($scope,$http) 
         ajaxPost('/AndSell/bubu/login/userLogin','LOGIN_ID='+$scope.userName+'&PWD='+$scope.password, function (response) {
             console.log(response);
             if (JSON.parse(response).code == 0) {
-                window.location.href='../main/main_index.html';
+                var isShop = JSON.parse(response).extraData.IS_SHOP;
+
+                if(undefined == isShop || isShop == null || isShop == "false"){
+                    window.location.href='../main/main_index.html';
+                }else{
+                    window.location.href='../main/main_shop.html';
+                }
+
             } else {
                 alert(JSON.parse(response).msg);
             }
