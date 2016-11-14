6894b05762cef384f1889335625161d637ba11c2
caoxingyu <caoxingyu2016@qq.com>
2016-11-15 01:45:03 +0800

    广告位商品显示

--- babuluo/web/h5/pages/home/controller.js
+++ babuluo/web/h5/pages/home/controller.js
@@ -10,10 +10,9 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
     $scope.recommendList = new Array();
     $scope.recommThreeList = new Array();
     $scope.recommNineList = new Array();
-    // $scope.bakingList = new Array();
-    // $scope.freezingList = new Array();
-    //$scope.BannerListMap=new Map();
-    $scope.BannerList=new Array();
+    $scope.BannerList = new Array();
+    var tagArray = new Array();
+
     //商品搜索
     $scope.searchPrd = function () {
         $state.go('pages/product/list', {keyword: $scope.prdKeyword});
@@ -25,7 +24,7 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
             weUI.toast.info('请先选择门店');
             $state.go('pages/shop');
         } else {
-            shopFactory.getShopById({'SHOP.SHOP_ID':id}, function (response) {
+            shopFactory.getShopById({'SHOP.SHOP_ID': id}, function (response) {
                 $scope.shopInfo = response.data[0];
                 if ($scope.shopInfo != undefined) {
                     setCookie('currentShopInfo', JSON.stringify($scope.shopInfo));
@@ -34,49 +33,129 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
         }
 
         var params = {}
-        params['SHOP_PRODUCT.TAG_ID'] = '1035,1036,1037';
+        params['SHOP_PRODUCT.TAG_ID'] = '1036,1037,3000,3001,3002,3003,3004,3005,3006';
         productFactory.getProductByTag(params, function (response) {
+            console.log(123);
             console.log(response);
             if (response.code == 0) {
-                $scope.tagProductList ={};
+                $scope.tagProductList = {};
                 response.data.forEach(function (ele) {
                     console.log(ele['SHOP_PRODUCT.TAG_ID']);
-                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('1035') >= 0) {
+                  /*  if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('1035') >= 0) {
                         if ($scope.tagProductList['1035'] == undefined) {
-                            var ne =  new Array;
+                            var ne = new Array;
                             ne.push(ele);
-                            $scope.tagProductList['1035']=ne;
+                            $scope.tagProductList['1035'] = ne;
                         } else {
                             var ne = $scope.tagProductList['1035'];
                             ne.push(ele);
-                            $scope.tagProductList['1035']=ne;
+                            $scope.tagProductList['1035'] = ne;
                         }
-                    }
+                    }*/
                     if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('1036') >= 0) {
                         if ($scope.tagProductList['1036'] == undefined) {
-                            var ne =  new Array;
+                            var ne = new Array;
                             ne.push(ele);
-                            $scope.tagProductList['1036']=ne;
+                            $scope.tagProductList['1036'] = ne;
                         } else {
                             var ne = $scope.tagProductList['1036'];
                             ne.push(ele);
-                            $scope.tagProductList['1036']=ne;
+                            $scope.tagProductList['1036'] = ne;
                         }
                     }
                     if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('1037') >= 0) {
                         if ($scope.tagProductList['1037'] == undefined) {
-                            var ne =  new Array;
+                            var ne = new Array;
                             ne.push(ele);
-                            $scope.tagProductList['1037']=ne;
+                            $scope.tagProductList['1037'] = ne;
                         } else {
                             var ne = $scope.tagProductList['1037'];
                             ne.push(ele);
-                            $scope.tagProductList['1037']=ne;
+                            $scope.tagProductList['1037'] = ne;
+                        }
+                    }
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3000') >= 0) {
+                        if ($scope.tagProductList['3000'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3000'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3000'];
+                            ne.push(ele);
+                            $scope.tagProductList['3000'] = ne;
+                        }
+                    }
+                   
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3001') >= 0) {
+                        if ($scope.tagProductList['3001'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3001'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3001'];
+                            ne.push(ele);
+                            $scope.tagProductList['3001'] = ne;
+                        }
+                    }
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3002') >= 0) {
+                        if ($scope.tagProductList['3002'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3002'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3002'];
+                            ne.push(ele);
+                            $scope.tagProductList['3002'] = ne;
+                        }
+                    }
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3003') >= 0) {
+                        if ($scope.tagProductList['3003'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3003'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3003'];
+                            ne.push(ele);
+                            $scope.tagProductList['3003'] = ne;
+                        }
+                    }
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3004') >= 0) {
+                        if ($scope.tagProductList['3004'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3004'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3004'];
+                            ne.push(ele);
+                            $scope.tagProductList['3004'] = ne;
+                        }
+                    }
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3005') >= 0) {
+                        if ($scope.tagProductList['3005'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3005'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3005'];
+                            ne.push(ele);
+                            $scope.tagProductList['3005'] = ne;
+                        }
+                    }
+                    if (ele['SHOP_PRODUCT.TAG_ID'].indexOf('3006') >= 0) {
+                        if ($scope.tagProductList['3006'] == undefined) {
+                            var ne = new Array;
+                            ne.push(ele);
+                            $scope.tagProductList['3006'] = ne;
+                        } else {
+                            var ne = $scope.tagProductList['3006'];
+                            ne.push(ele);
+                            $scope.tagProductList['3006'] = ne;
                         }
                     }
                 });
             }
-        });
+        });//
+
 
         // 设置轮播图图片间隔
         $scope.myInterval = 4000;
@@ -87,7 +166,7 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
             var dataList = response.data;
 
             dataList.forEach(function (ele) {
-                if (ele['BANNER.POSITION_ID'] =="1012") {   //首页
+                if (ele['BANNER.POSITION_ID'] == "1012") {   //首页
                     $scope.homeList.push(ele);
                 }
                 else if (ele['BANNER.POSITION_ID'] == "1013") {        //限时抢购
@@ -116,66 +195,84 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
                 else if (ele['BANNER.POSITION_ID'] == "1016") {     //今日推荐
                     $scope.recommendList.push(ele);
                 }
-               /* if (ele['BANNER.POSITION_ID'] == 1017) {     //今日推荐三连排
-                    $scope.recommThreeList.push(ele);
-                }*/
+                /* if (ele['BANNER.POSITION_ID'] == 1017) {     //今日推荐三连排
+                 $scope.recommThreeList.push(ele);
+                 }*/
                 else if (ele['BANNER.POSITION_ID'] == "1018") {     //今日推荐九宫格
                     $scope.recommNineList.push(ele);
                 }
-                else{
+                else {
                     console.log(ele['BANNER.POSITION_ID']);
-                    var flag=false;
-                    for(i=0;i<$scope.BannerList.length;i++){
-                        if ($scope.BannerList[i].id==ele['BANNER.POSITION_ID']){
+                    var flag = false;
+                    for (i = 0; i < $scope.BannerList.length; i++) {
+                        if ($scope.BannerList[i].id == ele['BANNER.POSITION_ID']) {
                             console.log(1);
-                            flag=true;
-                            var arrayList=$scope.BannerList[i].content;
+                            flag = true;
+                            var arrayList = $scope.BannerList[i].content;
                             arrayList.push(ele);
                         }
                     }
-                    if(flag==false) {
+                    if (flag == false) {
                         console.log(0);
                         var array = new Array();
                         array.push(ele);
 
                         listItem = new Object();
-                        listItem.id = ele['BANNER.POSITION_ID'];
+                        listItem.id = ele['BANNER.ID'];
                         listItem.name = ele['BANNER.NAME'];
                         listItem.content = array;
 
                         $scope.BannerList.push(listItem);
                     }
+                    //tagArray.push(ele['BANNER.TAG_ID']);
 
                 }
 
-                if (ele['BANNER.END_DATETIME']==null&&ele['BANNER.BEGIN_DATETIME']==null){
-                    ele['is_show']=true;
-                }else if(ele['BANNER.BEGIN_DATETIME']!=null){
+                if (ele['BANNER.END_DATETIME'] == null && ele['BANNER.BEGIN_DATETIME'] == null) {
+                    ele['is_show'] = true;
+                } else if (ele['BANNER.BEGIN_DATETIME'] != null) {
                     var startTime = new Date(ele['BANNER.BEGIN_DATETIME'].split('.')[0]).getTime();    //开始时间
                     var currentTime = new Date().getTime();      //当前时间 毫秒
-                    if(currentTime>startTime){
-                        if(ele['BANNER.BEGIN_DATETIME']==null){
-                            ele['is_show']=true;
+                    if (currentTime > startTime) {
+                        if (ele['BANNER.BEGIN_DATETIME'] == null) {
+                            ele['is_show'] = true;
                         }
-                        else{
+                        else {
                             var endTime = new Date(ele['BANNER.END_DATETIME'].split('.')[0]).getTime();      //结束时间
-                            if(currentTime<endTime){
-                                ele['is_show']=true;
+                            if (currentTime < endTime) {
+                                ele['is_show'] = true;
                             }
                         }
                     }
                 }
 
+            });
 
 
-            })
+           /* var params = {}
+            params['SHOP_PRODUCT.TAG_ID'] = tagArray.toString();
+            productFactory.getProductByTag(params, function (response) {
+                console.log(response);
+                if (response.code == 0) {
 
+                    response.data.forEach(function (ele) {
+                         dataList.forEach(function(banner){
+                              if(ele['SHOP_PRODUCT.TAG_ID'].indexOf(banner['BANNER.TAG_ID']) >= 0){
+                                  banner['PRODUCT_LIST']=
+
+                              }
+                         });
+
+
+                    });   //
+                }
+            });//*/
 
 
         });
-        console.log(789);
-      //console.log( $scope.BannerList);
+
     }
+
 
     //以毫秒为单位
     var fomtime = function (date, item) {
