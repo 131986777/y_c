3f30dced67ec9a0b1a18fa4fde740f3c6f2b1373
pabula <50054447@qq.com>
2016-11-14 20:25:21 +0800

    一问题

---
+++ babuluo/src/com/bubu/kucun/queryall/Command.java
@@ -0,0 +1,25 @@
+package com.bubu.kucun.queryall;
+import com.pabula.api.data.APIData;
+import com.pabula.api.data.ReturnData;
+import com.pabula.common.util.ValidateUtil;
+import com.pabula.fw.cmd.CommonCommand;
+import com.pabula.fw.exception.DataAccessException;
+import com.pabula.fw.exception.RuleException;
+
+/**  * Created by  xq on  2016-09-08.  */
+public class Command extends CommonCommand {
+    @Override
+    public void initdata(APIData apiData) {
+
+    }
+
+    @Override
+    public void validate(APIData apiData, ValidateUtil validate) throws RuleException {
+
+    }
+
+    @Override
+    public void execute(APIData apiData, ReturnData returnData) throws DataAccessException {
+
+    }
+}
