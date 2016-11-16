6dbdd62d080d0038db5b4279334739ffdc91d641
xq <269015753@qq.com>
2016-11-16 18:50:49 +0800

    dasfa

--- babuluo/web/h5/pages/home/controller.js
+++ babuluo/web/h5/pages/home/controller.js
@@ -249,28 +249,6 @@ angular.module('AndSell.H5.Main').controller('pages_home_Controller', function (
                 }
 
             });
-
-
-           /* var params = {}
-            params['SHOP_PRODUCT.TAG_ID'] = tagArray.toString();
-            productFactory.getProductByTag(params, function (response) {
-                console.log(response);
-                if (response.code == 0) {
-
-                    response.data.forEach(function (ele) {
-                         dataList.forEach(function(banner){
-                              if(ele['SHOP_PRODUCT.TAG_ID'].indexOf(banner['BANNER.TAG_ID']) >= 0){
-                                  banner['PRODUCT_LIST']=
-
-                              }
-                         });
-
-
-                    });   //
-                }
-            });//*/
-
-
         });
 
     }
