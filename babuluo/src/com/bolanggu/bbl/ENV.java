1655917658d2962baede423adcdb20701676748c
pabula <50054447@qq.com>
2016-11-16 22:14:32 +0800

    333

--- babuluo/src/com/bolanggu/bbl/ENV.java
+++ babuluo/src/com/bolanggu/bbl/ENV.java
@@ -6,8 +6,8 @@ package com.bolanggu.bbl;
 public class ENV {
 
     //模式
-    public static String STATE = "TEST";//测试模式
-    //public static String STATE = "NORMAL";//正常模式
+    //public static String STATE = "TEST";//测试模式
+    public static String STATE = "NORMAL";//正常模式
 
     //库存阀值
     public static Double STOCK_OFF = 0.75;
