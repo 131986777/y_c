439dc842e238dc4a129ce3f2cfe1c7df1b459589
xq <269015753@qq.com>
2016-11-17 16:17:08 +0800

    dasfa

--- babuluo/web/h5/pages/main/router.js
+++ babuluo/web/h5/pages/main/router.js
@@ -17,7 +17,7 @@ $import('pages/order/detail',{ORDER_ID: '1030'});
 $import('pages/order/addCoupon',{SKU_IDS: '1060',pickupPerson:'{}',PRODUCTS:'{}',MONEY:'0'});
 $import('pages/order/addAddress',{SKU_IDS: '1060'});
 
-$import('pages/personal');
+$import('pages/personal',{},true);
 $import('pages/personal/card');
 $import('pages/personal/coupon');
 
