94c12b35c0cff8405dc72f355073dcbe18c05b90
Yan <951554994@qq.com>
2016-11-18 17:10:03 +0800

    客户优惠券处理

--- babuluo/web/h5/pages/coupon/coupon-services.js
+++ babuluo/web/h5/pages/coupon/coupon-services.js
@@ -1,5 +1,7 @@
 AndSellH5MainModule.service('couponFactory', function ($resource) {
 
+    this.isLogin = $post($resource,'/login/isLogin');
+
     this.getCouponList = $post($resource,'/coupon/coupon/queryAllData');
 
     this.addMemberCoupon = $post($resource,'/member/coupon/add');
