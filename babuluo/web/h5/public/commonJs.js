e3fc22387091b60a0b2d5f6de3ed9b8f7622142c
snail <wyp402905456@hotmail.com>
2016-11-21 12:38:26 +0800

    商城问题样式

--- babuluo/web/h5/public/commonJs.js
+++ babuluo/web/h5/public/commonJs.js
@@ -298,16 +298,17 @@ function Map() {
 }
 
 var updateWxTitle = function (title) {
-    //var body = document.getElementsByTagName('body')[0];
-    //document.title = title;
-    //var iframe = document.createElement("iframe");
-    //iframe.setAttribute("src", "/empty.png");
+    // var body = document.getElementsByTagName('body')[0];
+    // document.title = title;
+    // var iframe = document.createElement("iframe");
+    // iframe.setAttribute("src", "/empty.png");
     //
-    //iframe.addEventListener('load', function() {
+    // iframe.addEventListener('load', function() {
     //    setTimeout(function() {
     //        iframe.removeEventListener('load');
     //        document.body.removeChild(iframe);
     //    }, 0);
-    //});
-    //document.body.appendChild(iframe);
+    // });
+    // document.body.appendChild(iframe);
+
 }
