e51973b80af1d2d110bf0f4df913557edbe85b6e
remix7 <remix7@live.cn>
2016-12-01 19:49:22 +0800

    create card and order analysis

---
+++ babuluo/web/app/card/card/cardAnalysis/controllers.js
@@ -0,0 +1,5 @@
+angular.module('AndSell.Main').controller('card_card_cardAnalysis_Controller', function ($scope, $stateParams,modalFactory,cardFactory) {
+
+    modalFactory.setTitle('会员卡消费分析');
+
+});
---
+++ babuluo/web/app/order/order/orderAnalysis/controllers.js
@@ -0,0 +1,6 @@
+/**
+ * Created by remix on 2016/12/1.
+ */
+angular.module('AndSell.Main').controller('order_order_orderDetail_Controller', function ($scope, $stateParams, orderFactory, modalFactory) {
+
+});
