81815dceece7da3ef4789e3bd839e3a15200e469
snail <wyp402905456@hotmail.com>
2016-12-05 17:04:02 +0800

    "分拣看板"页面

---
+++ babuluo/web/app/order/order/orderSorting/controllers.js
@@ -0,0 +1,6 @@
+angular.module('AndSell.Main').controller('order_order_orderSorting_Controller', function ($scope, $state, $stateParams, orderFactory, modalFactory) {
+
+    modalFactory.setTitle('分拣看板');
+    modalFactory.setBottom(false);
+
+});
