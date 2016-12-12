6cce8ad82d325ef66f046313806229be839cb466
remix7 <remix7@live.cn>
2016-12-12 12:59:49 +0800

    add compare Analysis

---
+++ babuluo/web/app/analysis/analysis/compareAnalysis/controllers.js
@@ -0,0 +1,7 @@
+/**
+ * Created by remix on 2016/12/12.
+ */
+angular.module('AndSell.Main').controller('analysis_analysis_compareAnalysis_Controller', function ($scope, $stateParams,$timeout, analysisFactory, modalFactory) {
+    modalFactory.setTitle("作站室");
+});
+
