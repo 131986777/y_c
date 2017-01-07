28306c1a258a02bce7a6eb02685ad6c2b547bd80
YanHanXuan <951554994@qq.com>
2017-01-07 11:27:11 +0800

    系统配置-显示优化 系统配置-修改

--- babuluo/web/app/sys/sys/sysSetting/controllers.js
+++ babuluo/web/app/sys/sys/sysSetting/controllers.js
@@ -1,61 +1,44 @@
 /**
  * Created by Vam W on 2017/1/5.
  */
-angular.module('AndSell.Main').controller('sys_sys_sysSetting_Controller', function ($scope, $stateParams, modalFactory, sysConfigFactory) {
+angular.module('AndSell.Main').controller('sys_sys_sysSetting_Controller', function ($scope, $state, $stateParams, modalFactory, sysConfigFactory) {
 
     modalFactory.setTitle('系统设置');
     modalFactory.setBottom(true);
 
     $scope.initLoad = function () {
-
+        $scope.configMap = new Map();
         sysConfigFactory.getSys({}, function (response) {
-            $scope.sysList = response.data;
-            console.log($scope.sysList);
-            $scope.sysList.forEach(function (ele) {
-                //支付设置
-                if (ele['SYS_CONFIG.CONFIG_KEY'] == "WX_APP_ID") {
-                    $scope.WX_APP_ID = ele['SYS_CONFIG.CONFIG_VALUE'];
-                }
-                if (ele['SYS_CONFIG.CONFIG_KEY'] == "WX_APP_SECRET") {
-                    $scope.WX_APP_SECRET = ele['SYS_CONFIG.CONFIG_VALUE'];
-                }
-                if (ele['SYS_CONFIG.CONFIG_KEY'] == "WX_MCHID") {
-                    $scope.WX_MCHID = ele['SYS_CONFIG.CONFIG_VALUE'];
-                }
-                if (ele['SYS_CONFIG.CONFIG_KEY'] == "WX_KEY") {
-                    $scope.WX_KEY = ele['SYS_CONFIG.CONFIG_VALUE'];
-                }
+            $scope.configMap = response.extraData.configMap;
+            //支付设置
+            $scope.WX_APP_ID = $scope.configMap["WX_APP_ID"];
+            $scope.WX_APP_SECRET = $scope.configMap["WX_APP_SECRET"];
+            $scope.WX_MCHID = $scope.configMap["WX_MCHID"];
+            $scope.WX_KEY = $scope.configMap["WX_KEY"];
+            //库存设置
+            $scope.INVENTORY_THRESHOLD = $scope.configMap["INVENTORY_THRESHOLD"];
+            //订单设置
+            $scope.ORDER_MODE = $scope.configMap["ORDER_MODE"];
 
-                //库存设置
-                if (ele['SYS_CONFIG.CONFIG_KEY'] == "INVENTORY_THRESHOLD") {
-                    $scope.INVENTORY_THRESHOLD = ele['SYS_CONFIG.CONFIG_VALUE'];
-                }
-
-                //订单设置
-                if (ele['SYS_CONFIG.CONFIG_KEY'] == "ORDER_MODE") {
-                    $scope.ORDER_MODE = ele['SYS_CONFIG.CONFIG_VALUE'];
-                }
-            });
         });
     };
     $scope.initLoad();
 
-
-    //modalFactory.showShortAlert("请输入0-100之间的正整数");
     //设置页面Bottom触发事件
     modalFactory.setBottom(true, function () {
-        console.log($scope.WX_APP_ID);
-        console.log($scope.WX_APP_SECRET);
-        console.log($scope.WX_MCHID);
-        console.log($scope.WX_KEY);
-        console.log($scope.INVENTORY_THRESHOLD);
-        console.log($scope.ORDER_MODE);
-        // memberFactory.modMemberListById($scope.member, function (response) {
-        //     modalFactory.showShortAlert("保存成功");
-        //     $scope.initLoad();
-        // }, function (response) {
-        //     modalFactory.showShortAlert(response.msg);
-        // });
+        var configList = [];
+        configList.push({"SYS_CONFIG.CONFIG_KEY": "WX_APP_ID", "SYS_CONFIG.CONFIG_VALUE": $scope.WX_APP_ID});
+        configList.push({"SYS_CONFIG.CONFIG_KEY": "WX_APP_SECRET", "SYS_CONFIG.CONFIG_VALUE": $scope.WX_APP_SECRET});
+        configList.push({"SYS_CONFIG.CONFIG_KEY": "WX_MCHID", "SYS_CONFIG.CONFIG_VALUE": $scope.WX_MCHID});
+        configList.push({"SYS_CONFIG.CONFIG_KEY": "WX_KEY", "SYS_CONFIG.CONFIG_VALUE": $scope.WX_KEY});
+        configList.push({"SYS_CONFIG.CONFIG_KEY": "INVENTORY_THRESHOLD", "SYS_CONFIG.CONFIG_VALUE": $scope.INVENTORY_THRESHOLD});
+        configList.push({"SYS_CONFIG.CONFIG_KEY": "ORDER_MODE", "SYS_CONFIG.CONFIG_VALUE": $scope.ORDER_MODE});
+        console.log(configList);
+        configList.forEach(function (ele) {
+            sysConfigFactory.modSysByKey(ele, function (response) {
+            });
+        });
+        modalFactory.showShortAlert("修改成功");
     }, function () {
         //取消事件
         $state.go('order/order/orderList//3');
