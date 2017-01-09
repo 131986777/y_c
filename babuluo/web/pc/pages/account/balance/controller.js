130874a58a021b95e5729168592a5c88f5c9801f
YanHanXuan <951554994@qq.com>
2017-01-09 14:56:06 +0800

    pc资金明细

--- babuluo/web/pc/pages/account/balance/controller.js
+++ babuluo/web/pc/pages/account/balance/controller.js
@@ -1,9 +1,52 @@
-angular.module('AndSell.PC.Main').controller('pages_account_balance_Controller', function (productFactory, $interval, $scope, $state, modalFactory, shopFactory) {
+angular.module('AndSell.PC.Main').controller('pages_account_balance_Controller', function (productFactory, $interval, $scope, $state, modalFactory, balanceFactory) {
 
     modalFactory.setTitle("账户明细");
 
     modalFactory.setHeader(false);
 
     modalFactory.setSide(true);
+
+    $scope.balanceDetail = "收支明细";
+    $scope.initLoad  = function () {
+        $scope.uid = getCookie('ANDSELLID');
+        $scope.queryFinanceListByUid($scope.uid);
+    }
+
+    $scope.queryFinanceListByUid = function(uid){
+        var form = {};
+        form['FINANCE_LIST.USER_ID'] = uid;
+        balanceFactory.queryByUid(form, function (response) {
+            $scope.balanceInfo = response.data;
+            $scope.balance = $scope.balanceInfo[0]['FINANCE_LIST.BALANCE'];
+            $scope.typeInfo = $scope.balanceInfo;
+        })
+    }
+
+    $scope.initLoad();
+
+    $scope.chooseType = function (type){
+        console.log(type);
+        if(type == 'null'){
+            $scope.balanceInfo = $scope.typeInfo;
+            $scope.balanceDetail = "收支明细";
+        }
+        else{
+            if(type == 'decrease'){
+                $scope.balanceDetail = "支出";
+            }
+            if(type == 'increase'){
+                $scope.balanceDetail = "收入";
+            }
+            $scope.balanceInfo =[];
+            var array = new Array();
+            $scope.typeInfo.forEach(function (ele) {
+                if(ele['FINANCE_LIST.CHANGE_TYPE'] == type){
+                    array.push(ele);
+                }
+            })
+            $scope.balanceInfo = array;
+        }
+        return $scope.balanceInfo;
+    }
 
 });
