625546471ae28e12fdac9b2eb51c45fbfca546f9
YanHanXuan <951554994@qq.com>
2017-01-09 15:07:41 +0800

    pc资金明细

--- babuluo/web/pc/pages/account/balance/controller.js
+++ babuluo/web/pc/pages/account/balance/controller.js
@@ -1,4 +1,4 @@
-angular.module('AndSell.PC.Main').controller('pages_account_balance_Controller', function (productFactory, $interval, $scope, $state, modalFactory, shopFactory) {
+angular.module('AndSell.PC.Main').controller('pages_account_balance_Controller', function (productFactory, $interval, $scope, $state, modalFactory, balanceFactory) {
 
     modalFactory.setTitle("账户明细");
 
