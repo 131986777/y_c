1e1f05c1852cdbf5ab57d9fc319476d55ac04d34
sunlily <18251815890@163.com>
2017-01-13 17:50:11 +0800

    门店选中

--- babuluo/web/pc/pages/shop/controller.js
+++ babuluo/web/pc/pages/shop/controller.js
@@ -3,4 +3,25 @@ angular.module('AndSell.PC.Main').controller('pages_shop_Controller', function (
     modalFactory.setTitle("订单列表");
 
     modalFactory.setHeader(false);
+    $(".store").click(function () {
+        $(".store").removeClass("active");
+        $(".yang").removeClass("choosed");
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).find(".yang").removeClass("choosed");
+        } else {
+            $(this).addClass("active");
+            $(this).find(".yang").addClass("choosed");
+        }
+    });
+    $(".storeArea").click(function () {
+
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this).find(".yang").removeClass("choosed");
+        } else {
+            $(this).addClass("active");
+            $(this).find(".yang").addClass("choosed");
+        }
+    });
 });
