9a90e5a163bfad9a2c31bbe6e4da3e0827e8e8a9
xq <269015753@qq.com>
2017-01-20 10:52:09 +0800

    调整

--- babuluo/web/pc/pages/product/collect/controller.js
+++ babuluo/web/pc/pages/product/collect/controller.js
@@ -1,6 +1,3 @@
-/**
- * Created by njwb on 2017/1/17.
- */
 angular.module('AndSell.PC.Main').controller('pages_product_collect_Controller', function (productFactory, $interval, $stateParams, $scope, $state, modalFactory) {
 
     modalFactory.setTitle("我的收藏");
