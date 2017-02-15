d8eb0db7669854474afe48ab0f7b37eccf05b921
remix7 <remix7@live.cn>
2017-02-15 20:19:43 +0800

    添加来客数分析

--- babuluo/web/app/analysis/analysis-router.js
+++ babuluo/web/app/analysis/analysis-router.js
@@ -2,18 +2,20 @@
  * Created by liutao on 2016/12/8 18:0:0.
  */
 //日报相关
-$import('analysis/analysis/dailyAnalysis',undefined,true);
-$import('analysis/analysis/offlineDailyAnalysis',undefined,true);
+$import('analysis/analysis/dailyAnalysis', undefined, true);
+$import('analysis/analysis/offlineDailyAnalysis', undefined, true);
 //会员分析相关
-$import('analysis/analysis/cardAnalysis',undefined,true);
+$import('analysis/analysis/cardAnalysis', undefined, true);
 //订单相关
-$import('analysis/analysis/orderAnalysis',undefined,true);
-$import('analysis/analysis/offlineOrderAnalysis',undefined,true);
+$import('analysis/analysis/orderAnalysis', undefined, true);
+$import('analysis/analysis/offlineOrderAnalysis', undefined, true);
 //作战室相关
-$import('analysis/analysis/compareAnalysis',undefined,true);
+$import('analysis/analysis/compareAnalysis', undefined, true);
 //直播相关
-$import('analysis/analysis/liveAnalysis',undefined,true);
+$import('analysis/analysis/liveAnalysis', undefined, true);
 
 //
-$import('analysis/analysis/offlineFormAnalysis',undefined,true);
-$import('analysis/analysis/formAnalysis',undefined,true);
+$import('analysis/analysis/offlineFormAnalysis', undefined, true);
+$import('analysis/analysis/formAnalysis', undefined, true);
+//来客分析
+$import('analysis/analysis/guestAnalysis', undefined, true);
