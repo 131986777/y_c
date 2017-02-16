0526f59cd4896654d14d228f74e8a453c0585e96
remix7 <remix7@live.cn>
2017-02-16 11:31:37 +0800

    添加会员卡分析

--- babuluo/web/app/analysis/analysis/offlineFormAnalysis/controllers.js
+++ babuluo/web/app/analysis/analysis/offlineFormAnalysis/controllers.js
@@ -27,6 +27,8 @@ angular.module('AndSell.Main').controller('analysis_analysis_offlineFormAnalysis
             }
             var flag = JSON.parse((response.data)[0]['MANAGE_DATA_ANALYSIS.SOURCE']);
             var add_card = 0;
+            var new_card_vip = 0;
+            var new_card_pt = 0;
             var money_count = 0;
             var order_count = 0;
             var guest_count = 0;
@@ -35,7 +37,7 @@ angular.module('AndSell.Main').controller('analysis_analysis_offlineFormAnalysis
             var vip_card_money = 0;
             var total_unit = 0;
             var money_discount = 0;
-            var hand_discount = 0;
+            // var hand_discount = 0;
             var auto_discount = 0;
             for (var j = 0; j < (response.data).length; j++) {
                 var json = JSON.parse((response.data)[j]['MANAGE_DATA_ANALYSIS.SOURCE']);
@@ -49,9 +51,15 @@ angular.module('AndSell.Main').controller('analysis_analysis_offlineFormAnalysis
                     if (parseInt(json[i]['SHOP_VALUE']['GUEST_COUNT']) >= 0) {
                         guest_count += parseInt(json[i]['SHOP_VALUE']['GUEST_COUNT']);
                     }
+                    if (parseInt(json[i]['SHOP_VALUE']['NEW_CARD_VIP']) >= 0) {
+                        new_card_vip += parseInt(json[i]['SHOP_VALUE']['NEW_CARD_VIP']);
+                    }
+                    if (parseInt(json[i]['SHOP_VALUE']['NEW_CARD_PT']) >= 0) {
+                        new_card_pt += parseInt(json[i]['SHOP_VALUE']['NEW_CARD_PT']);
+                    }
                     money_count += parseInt(json[i]['SHOP_VALUE']['MONEY_COUNT']) / 100;
                     money_discount += parseInt(json[i]['SHOP_VALUE']['MONEY_DISCOUNT']) / 100;
-                    hand_discount += parseInt(json[i]['SHOP_VALUE']['HAND_DISCOUNT']) / 100;
+                    // hand_discount += parseInt(json[i]['SHOP_VALUE']['HAND_DISCOUNT']) / 100;
                     auto_discount += parseInt(json[i]['SHOP_VALUE']['AUTO_DISCOUNT']) / 100;
                 }
                 if (j != 0) {
@@ -66,9 +74,15 @@ angular.module('AndSell.Main').controller('analysis_analysis_offlineFormAnalysis
                         if (parseInt(flag[i]['SHOP_VALUE']['GUEST_COUNT']) >= 0 && parseInt(json[i]['SHOP_VALUE']['GUEST_COUNT']) >= 0) {
                             flag[i]['SHOP_VALUE']['GUEST_COUNT'] = parseInt(flag[i]['SHOP_VALUE']['GUEST_COUNT']) + parseInt(json[i]['SHOP_VALUE']['GUEST_COUNT']);
                         }
+                        if (parseInt(flag[i]['SHOP_VALUE']['NEW_CARD_VIP']) >= 0 && parseInt(json[i]['SHOP_VALUE']['NEW_CARD_VIP']) >= 0) {
+                            flag[i]['SHOP_VALUE']['NEW_CARD_VIP'] = parseInt(flag[i]['SHOP_VALUE']['NEW_CARD_VIP']) + parseInt(json[i]['SHOP_VALUE']['NEW_CARD_VIP']);
+                        }
+                        if (parseInt(flag[i]['SHOP_VALUE']['NEW_CARD_PT']) >= 0 && parseInt(json[i]['SHOP_VALUE']['NEW_CARD_PT']) >= 0) {
+                            flag[i]['SHOP_VALUE']['NEW_CARD_PT'] = parseInt(flag[i]['SHOP_VALUE']['NEW_CARD_PT']) + parseInt(json[i]['SHOP_VALUE']['NEW_CARD_PT']);
+                        }
                         flag[i]['SHOP_VALUE']['TOTAL_UNIT'] = parseFloat(flag[i]['SHOP_VALUE']['TOTAL_UNIT']) + parseFloat(json[i]['SHOP_VALUE']['TOTAL_UNIT']);
                         flag[i]['SHOP_VALUE']['MONEY_DISCOUNT'] = parseInt(flag[i]['SHOP_VALUE']['MONEY_DISCOUNT']) + parseInt(json[i]['SHOP_VALUE']['MONEY_DISCOUNT']);
-                        flag[i]['SHOP_VALUE']['HAND_DISCOUNT'] = parseInt(flag[i]['SHOP_VALUE']['HAND_DISCOUNT']) + parseInt(json[i]['SHOP_VALUE']['HAND_DISCOUNT']);
+                        // flag[i]['SHOP_VALUE']['HAND_DISCOUNT'] = parseInt(flag[i]['SHOP_VALUE']['HAND_DISCOUNT']) + parseInt(json[i]['SHOP_VALUE']['HAND_DISCOUNT']);
                         flag[i]['SHOP_VALUE']['AUTO_DISCOUNT'] = parseInt(flag[i]['SHOP_VALUE']['AUTO_DISCOUNT']) + parseInt(json[i]['SHOP_VALUE']['AUTO_DISCOUNT']);
 
 
@@ -85,12 +99,14 @@ angular.module('AndSell.Main').controller('analysis_analysis_offlineFormAnalysis
                 flag[i]['SHOP_VALUE']['AVG_ORDER_MONEY'] = parseFloat(flag[i]['SHOP_VALUE']['MONEY_COUNT']) / flag[i]['SHOP_VALUE']['ORDER_COUNT'];
             }
             $scope.ADD_CARD = add_card;
+            $scope.NEW_CARD_VIP = new_card_vip;
+            $scope.NEW_CARD_PT = new_card_pt;
             $scope.MONEY_DISCOUNT = money_discount
             $scope.TOTAL_CARD_MONEY = total_card_money;
             $scope.PT_CARD_MONEY = pt_card_money;
             $scope.VIP_CARD_MONEY = vip_card_money;
             $scope.TOTAL_UNIT = total_unit;
-            $scope.HAND_DISCOUNT = hand_discount;
+            // $scope.HAND_DISCOUNT = hand_discount;
             $scope.ORDER_COUNT = order_count;
             $scope.GUEST_COUNT = guest_count;
             $scope.MONEY_COUNT = money_count;
@@ -103,22 +119,24 @@ angular.module('AndSell.Main').controller('analysis_analysis_offlineFormAnalysis
     }
 
     function clearTable() {
-        $scope.ADD_CARD = "";
-        $scope.MONEY_DISCOUNT = "";
-        $scope.AVG_MONEY_COUNT = "";
-        $scope.TOTAL_CARD_MONEY = "";
-        $scope.PT_CARD_MONEY = "";
-        $scope.VIP_CARD_MONEY = "";
-        $scope.AVG_ORDER_MONEY = "";
-        $scope.AVG_UNIT_MONEY = "";
-        $scope.TOTAL_UNIT = "";
-        $scope.MONEY_DISCOUNT = "";
-        $scope.HAND_DISCOUNT = "";
-        $scope.ORDER_COUNT = "";
-        $scope.GUEST_COUNT = "";
-        $scope.MONEY_COUNT = "";
-        $scope.AUTO_DISCOUNT = "";
-        $scope.FORMSOURCE = "";
+        $scope.ADD_CARD = 0;
+        $scope.NEW_CARD_VIP = 0;
+        $scope.NEW_CARD_PT = 0;
+        $scope.MONEY_DISCOUNT = 0;
+        $scope.AVG_MONEY_COUNT = 0;
+        $scope.TOTAL_CARD_MONEY = 0;
+        $scope.PT_CARD_MONEY = 0;
+        $scope.VIP_CARD_MONEY = 0;
+        $scope.AVG_ORDER_MONEY = 0;
+        $scope.AVG_UNIT_MONEY = 0;
+        $scope.TOTAL_UNIT = 0;
+        $scope.MONEY_DISCOUNT = 0;
+        // $scope.HAND_DISCOUNT = 0;
+        $scope.ORDER_COUNT = 0;
+        $scope.GUEST_COUNT = 0;
+        $scope.MONEY_COUNT = 0;
+        $scope.AUTO_DISCOUNT = 0;
+        $scope.FORMSOURCE = null;
     }
 });
 //获取昨天
