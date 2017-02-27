dd6ec8e60072032bf3edeae558372a15ad9a4404
xq <269015753@qq.com>
2017-02-27 12:08:14 +0800

    秒杀优化

--- babuluo/web/h5/pages/home/home-services.js
+++ babuluo/web/h5/pages/home/home-services.js
@@ -2,4 +2,7 @@ AndSellH5MainModule.service('seckillFactory', function (http) {
     this.queryByStateAndTime=http.post('/promo/seckill/myInterface/indexQuerySeckill');
 
     this.goSeckill=http.post('/promo/seckill/myInterface/goSeckill');
+
+    this.backspaceSeckill=http.post('/promo/seckill/myInterface/backspaceOrder');
+
 })
--- babuluo/web/pc/pages/order/confirmSeckill/controller.js
+++ babuluo/web/pc/pages/order/confirmSeckill/controller.js
@@ -79,25 +79,6 @@ angular.module('AndSell.PC.Main').controller('pages_order_confirmSeckill_Control
      * 秒杀是否成功
      */
     $scope.goSeckill=function(){
-        var form = {};
-        form['SECKILL_ID']=$scope.seckill['seckill_id'];
-        form['NUM']=1;
-        var memberId=getCookie("ANDSELLID");
-        form['MEMBER_ID']=memberId;
-        seckillFactory.goSeckill(form,function(response){
-            var promoReturn = response['extraData']['promoReturn'];
-            //成功就下单 不成功提示信息
-            if (promoReturn['state']==0){
-                $scope.commitOrder();
-            }else if (promoReturn['state']==1){
-                alert(promoReturn['message']);
-            }
-        })
-    }
-
-    //提交订单
-    $scope.commitOrder = function () {
-
         if (!$scope.canCommit) {
             return;
         }
@@ -112,11 +93,31 @@ angular.module('AndSell.PC.Main').controller('pages_order_confirmSeckill_Control
             return;
         }
 
+        var form = {};
+        form['SECKILL_ID']=$scope.seckill['seckill_id'];
+        form['NUM']=1;
+        var memberId=getCookie("ANDSELLID");
+        form['MEMBER_ID']=memberId;
+        seckillFactory.goSeckill(form,function(response){
+            var promoReturn = response['extraData']['promoReturn'];
+            //成功就下单 不成功提示信息
+            if (promoReturn['state']==0){
+                $scope.commitOrder($scope.seckill['seckill_id'], 1 ,memberId);
+            }else if (promoReturn['state']==1){
+                alert(promoReturn['message']);
+            }
+        })
+    }
+
+    //提交订单
+    $scope.commitOrder = function (seckillId,num ,memberId) {
+
         if ($scope.commitClick) {
             $scope.commitClick = false;
 
             var params = $scope.order;
 
+            params['SHOP_ORDER.SPECIAL_MODEL'] = "SECKILL_"+seckillId;//秒杀的ID
             params['SHOP_ORDER.REC_CONTACT'] = $scope.pickMan.man;//收货人
             params['SHOP_ORDER.REC_PHONE'] = $scope.pickMan.phone;//联系电话
             params['SHOP_ORDER.REC_TYPE'] = 2;//收货方式为自提
@@ -141,6 +142,14 @@ angular.module('AndSell.PC.Main').controller('pages_order_confirmSeckill_Control
                     + response.extraData.ORDER_ID);
 
             }, function (response) {
+                //添加订单失败的话就要回退秒杀！
+                var form = {};
+                form['SECKILL_ID']=seckillId;
+                form['MEMBER_ID']=memberId;
+                form['NUM']=num;
+                seckillFactory.backspaceSeckill(form,function(response){
+                })
+
                 $scope.commitClick = true;
                 modalFactory.showShortAlert(response.msg);
             });
