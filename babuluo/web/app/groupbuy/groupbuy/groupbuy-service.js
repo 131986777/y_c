39738c82bcda7e5e9ede745d422434dbad827973
liutao <echoesgml@gmail.com>
2017-04-14 18:06:58 +0800

    搭建团购pc

---
+++ babuluo/web/app/groupbuy/groupbuy/groupbuy-service.js
@@ -0,0 +1,8 @@
+/**
+ * Created by liutao on 2017/4/13.
+ */
+AndSellMainModule.service('groupBuyPlanFactory', function (http) {
+    this.addGroupBuyPlan = http.post("/group/buy/plan/add");
+    this.queryAllGroupBuyPlan = http.post("/group/buy/plan/queryAll");
+    this.modifyById = http.post("/group/buy/plan/modifyById");
+});
