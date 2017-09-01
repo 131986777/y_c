a6dd27f5174dc073a6322303e5589f4ec9866fac
1127204156@qq.com <766003097@qq.com>
2017-09-01 17:14:43 +0800

    积分兑换  前端提交 Signed-off-by: 1127204156@qq.com <766003097@qq.com>

---
+++ babuluo/web/h5/pages/account/details/controller.js
@@ -0,0 +1,11 @@
+angular.module('AndSell.H5.Main').controller('pages_account_details_Controller', function ($scope, $state, $stateParams,modalFactory, balanceFactory,weUI) {
+
+
+	   modalFactory.setTitle("详情");
+	    modalFactory.setBottom(false);
+   
+});
+
+
+
+
---
+++ babuluo/web/h5/pages/account/integral/controller.js
@@ -0,0 +1,12 @@
+angular.module('AndSell.H5.Main').controller('pages_account_integral_Controller', function ($scope, $state, $stateParams, modalFactory, balanceFactory,weUI) {
+
+	   modalFactory.setTitle("积分兑换");
+	    modalFactory.setBottom(false);
+
+	 
+   
+});
+
+
+
+
