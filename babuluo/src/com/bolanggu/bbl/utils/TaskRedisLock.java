2cbead279febda7234d79b75b52f188f46a42c99
蒋会勇 <@DESKTOP-HT65K7D>
2018-02-11 10:43:02 +0800

    Signed-off-by: 蒋会勇 <蒋会勇@DESKTOP-HT65K7D>

--- babuluo/src/com/bolanggu/bbl/utils/TaskRedisLock.java
+++ babuluo/src/com/bolanggu/bbl/utils/TaskRedisLock.java
@@ -3,7 +3,7 @@ package com.bolanggu.bbl.utils;
 public class TaskRedisLock {
 
 	public static void redisLock(String key) throws Exception{
-		long result = RedisCache.getInstance().setnx("UpdateOrderPay", "1", 60);
+		long result = RedisCache.getInstance().setnx(key, "1", 60);
     	if(result == 0){
     		throw new Exception("============redislock=============");
     	}
